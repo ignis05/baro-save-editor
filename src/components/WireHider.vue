c4a88e289492b0214a81f287dc5acfeff50e4c38
ignis05 <48099798+ignis05@users.noreply.github.com>
2021-07-31 01:54:17 +0200

    Added WireHider functionality

--- src/components/WireHider.vue
+++ src/components/WireHider.vue
@@ -3,12 +3,60 @@
     <v-card-header class="flex-column align-center">
       <v-card-title> Wire Hider </v-card-title>
     </v-card-header>
-    <v-sheet class="mb-2"> </v-sheet>
+    <v-sheet class="mb-2 px-4 d-flex flex-row justify-center">
+      <select v-model="selectedWireType" class="px-2">
+        <option value="all">all</option>
+        <option value="whitewire">white</option>
+        <option value="redwire">red</option>
+        <option value="bluewire">blue</option>
+        <option value="yellowwire">yellow</option>
+        <option value="greenwire">green</option>
+        <option value="blackwire">black</option>
+        <option value="brownall">brown</option>
+      </select>
+      <v-btn @click="hideWires" variant="outlined" color="secondary" class="mx-2">Hide</v-btn>
+      <v-btn @click="unhideWires" variant="outlined" color="secondary">Unhide</v-btn>
+    </v-sheet>
   </v-card>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      selectedWireType: 'all',
+    }
+  },
+  methods: {
+    hideWires(setToBool = true) {
+      var setTo = setToBool ? 'True' : 'False'
+      var count = 0
+      for (let wire of this.$store.getters.sub.elements.filter((el) => this.matchWireType(el.attributes.identifier))) {
+        if (wire.attributes.hiddeningame != setTo) {
+          wire.attributes.hiddeningame = setTo
+          count++
+        }
+      }
+      // todo: v-alert `Set ${count} wires to ${setToBool ? 'hidden' : 'visible'}`
+    },
+    unhideWires() {
+      this.hideWires(false)
+    },
+    matchWireType(identifier) {
+      if (this.selectedWireType === 'all') return /^.*wire$/.test(identifier)
+      return identifier === this.selectedWireType
+    },
+  },
+}
 </script>
 
-<style></style>
+<style scoped>
+select {
+  border: 1px solid white;
+  color: white;
+  cursor: pointer;
+}
+option {
+  background: #212121;
+}
+</style>
