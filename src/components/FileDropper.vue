007f64ed2424d61dbfa23b3f4844c60987b56a83
ignis05 <48099798+ignis05@users.noreply.github.com>
2021-08-02 01:30:01 +0200

    Added help buttons to FileDropper

--- src/components/FileDropper.vue
+++ src/components/FileDropper.vue
@@ -1,9 +1,112 @@
 <template>
   <v-card class="pa-4" elevation="1">
     <div class="d-flex flex-row align-center justify-center mb-1">
-      <div class="h3 mb-1">Drag a file below to get started</div>
+      <div class="h3 mb-1">Drag a file below to load it:</div>
       <v-spacer></v-spacer>
-      <v-btn size="x-small" icon title="where can i find my files"><v-icon>mdi-help-circle-outline</v-icon></v-btn>
+      <!-- locate file dialog -->
+      <v-btn @click.stop="fileLocDialog = true" size="x-small" icon title="Where can I find my files?">
+        <v-icon>mdi-file-find-outline</v-icon>
+      </v-btn>
+      <v-dialog v-model="fileLocDialog">
+        <v-card>
+          <v-card-title>
+            <span class="text-h5">Where are my {{ fromSubEditor ? '.sub' : '.save' }} files?</span>
+            <v-spacer></v-spacer>
+            <v-btn color="red" size="x-small" icon @click="fileLocDialog = false">
+              <v-icon>mdi-close-thick</v-icon>
+            </v-btn>
+          </v-card-title>
+          <v-divider></v-divider>
+          <!-- sub editor -->
+          <v-card-text class="pl-8" v-if="fromSubEditor">
+            <div class="text-h6 text-center">Submarines are within Barotrauma installation dir:</div>
+            <ul>
+              <li>
+                Created or modified by you:
+                <span class="text-primary"> Barotrauma/Submarines </span>
+              </li>
+              <li>
+                Downloaded when joining server:
+                <span class="text-primary"> Barotrauma/Submarines/Downloaded </span> (it's a hidden directory)
+              </li>
+              <li>
+                Downloaded from Steam Workshop:
+                <span class="text-primary"> Barotrauma/Mods/[mod name] </span>
+              </li>
+            </ul>
+            <br />
+            <p class="text">
+              To open your Barotrauma dir, right click on it in Steam menu and select "Browse Local Files"
+            </p>
+            <v-img src="~@/assets/browseLocalFiles.png"></v-img>
+          </v-card-text>
+          <!-- save editor -->
+          <v-card-text class="pl-8" v-else>
+            <div class="text-h6 text-center">Save location depends on your OS:</div>
+            <ul>
+              <li>
+                Windows:
+                <span class="text-primary">
+                  C:\Users\%username%\AppData\Local\Daedalic Entertainment GmbH\Barotrauma
+                </span>
+              </li>
+              <li>
+                Linux:
+                <span class="text-primary"> /home/$USER/.local/share/Daedalic Entertainment GmbH/ </span>
+              </li>
+              <li>
+                MacOS:
+                <span class="text-primary"> /$USER/Library/Application Support/Daedalic Entertainment GmbH/ </span>
+              </li>
+            </ul>
+          </v-card-text>
+        </v-card>
+      </v-dialog>
+      <!-- help dialog -->
+      <v-btn @click.stop="helpDialog = true" size="x-small" icon title="What files can I load?">
+        <v-icon>mdi-help-circle-outline</v-icon>
+      </v-btn>
+      <v-dialog v-model="helpDialog">
+        <v-card>
+          <v-card-title>
+            <span class="text-h5">What files can i load here?</span>
+            <v-spacer></v-spacer>
+            <v-btn color="red" size="x-small" icon @click="helpDialog = false">
+              <v-icon>mdi-close-thick</v-icon>
+            </v-btn>
+          </v-card-title>
+          <v-divider></v-divider>
+          <!-- sub editor -->
+          <v-card-text class="pl-8" v-if="fromSubEditor">
+            <div class="text-h6 text-center">Submarine editor supports following files:</div>
+            <ul>
+              <li><span class="text-primary font-weight-bold">.sub</span> - Barotrauma submarine files</li>
+              <li><span class="text-primary text-bold">.xml</span> - Decompressed Barotrauma submarine files</li>
+            </ul>
+          </v-card-text>
+          <!-- save editor -->
+          <v-card-text class="pl-8" v-else>
+            <div class="text-h6 text-center">
+              Save editor needs to load <span class="text-primary font-weight-bold">.save</span> file first, then it
+              accepts:
+            </div>
+            <ul>
+              <li>
+                <span class="text-primary font-weight-bold">.sub</span> - Add a new owned submarine or update file of an
+                existing one
+              </li>
+              <li>
+                <span class="text-primary font-weight-bold">gamesession.xml</span> - Replace gamesession.xml inside the
+                loaded save
+              </li>
+              <li>
+                <span class="text-primary font-weight-bold">(...)_CharacterData.xml</span> - Import player characters
+                from multiplayer save as bots
+              </li>
+            </ul>
+          </v-card-text>
+        </v-card>
+      </v-dialog>
     </div>
     <div
       class="dropzone"
@@ -35,6 +138,8 @@ export default {
   data() {
     return {
       hovered: false,
+      fileLocDialog: false,
+      helpDialog: false,
     }
   },
   methods: {
@@ -110,5 +215,12 @@ input.dropzoneInput {
   position: relative;
   background-image: url('~@/assets/file-upload-outline.png');
   background-position: center;
+}
+</style>
+
+<style>
+.v-overlay__content {
+  max-width: 780px !important;
+  max-height: 650px !important;
 }
 </style>
