f9fbcc925b22f8ee786361eb9f0537c3180057f4
ignis05 <48099798+ignis05@users.noreply.github.com>
2021-08-06 18:52:20 +0200

    Added note to About page

--- src/views/About.vue
+++ src/views/About.vue
@@ -12,8 +12,27 @@
         </v-sheet>
       </v-col>
     </v-row>
+    <v-row>
+      <v-col>
+        <v-sheet elevation="1" class="pa-2" style="text-align: center">
+          <b>
+            Note: Always back up the original file before replacing it with the downloaded one.<br />
+            Even if something had worked for you before, there is always a risk that a game update breaks some stuff.
+          </b>
+        </v-sheet>
+      </v-col>
+    </v-row>
     <AboutSubEditor v-if="subEditor"></AboutSubEditor>
     <AboutSaveEditor v-else></AboutSaveEditor>
+    <v-col>
+      <v-sheet elevation="1" class="pa-2" style="text-align: center">
+        If the editor freezes, crashes or outputs corrupted file feel free to create a
+        <a style="color: rgb(var(--v-theme-secondary))" href="https://github.com/ignis05/baro-save-editor/issues">
+          github issue
+        </a>
+        attaching original file(s) and providing steps for reproduction.
+      </v-sheet>
+    </v-col>
   </v-container>
 </template>
 
