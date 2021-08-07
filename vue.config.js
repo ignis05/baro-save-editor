169844dc60d2d742bebec1fa63c406e6189d3efb
ignis05 <48099798+ignis05@users.noreply.github.com>
2021-08-07 18:39:20 +0200

    chore: change default webpage title

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? '/baro-save-editor/' : '/',
   transpileDependencies: ['vuetify'],
+  chainWebpack: (config) => {
+    config.plugin('html').tap((args) => {
+      args[0].title = 'Barotrauma Save Editor'
+      return args
+    })
+  },
 }
